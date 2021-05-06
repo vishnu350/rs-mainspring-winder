075c138aabe3beeca050ebd4538181e2d869c5bd
Vishnu P. Nambiar <5123344+vishnu350@users.noreply.github.com>
2021-05-07 01:10:04 +0800

    First version of functional generator for creating winders of different sizes.

--- generate_stl.py
+++ generate_stl.py
@@ -2,62 +2,42 @@ FREECADPATH = '/usr/lib/freecad/lib'
 import sys
 sys.path.append(FREECADPATH)
 from collections import defaultdict
+import FreeCAD
+import Mesh
 
-def convert_model(filename, arclen, radius, width, height):
-    try:
-        import FreeCAD
-        from FreeCAD import Vector
-    except ValueError:
-        print ('import error\n')
+def export_stl(body_obj,filename):
+    __objs__=[]
+    __objs__.append(FreeCAD.getDocument("rs_winder").getObject(body_obj))
+    Mesh.export(__objs__,filename)
+    del __objs__
+
+## List of sizes to generate
+bowl_d = ["9.0", "9.5", "10.0", "10.5", "11.0", "11.5", "12.0", "12.5", "13.0", "13.5"]
+spring_d = ["8.0", "8.5", "9.0", "9.5", "10.0", "10.5", "11.0", "11.5", "12.0", "12.5"]
+
+## Open project file and spreadsheet
+FreeCAD.openDocument("rs-winder.FCStd")
+doc = App.ActiveDocument
+sheet = doc.Spreadsheet002
+
+## Generate winder base
+export_stl("Body002", "rs-winder-base-stdsize.stl")
+
+## Generate housing/plunger (based on spring diameter)
+for n in spring_d:
+    sheet.set("spr_d", n)
+    sheet.set("version", n+"MM")
+    if len(n) < 4:
+        sheet.set("version_x_offs", "-5.20")
     else:
-        FreeCAD.open(filename)
+        sheet.set("version_x_offs", "-6.30")
+    doc.recompute(None,True,True)
+    export_stl("Body", "rs-winder-plunger-"+n+"mm.stl")
+    export_stl("Body001", "rs-winder-housing-"+n+"mm.stl")
 
-        doc = App.ActiveDocument
-
-        sheet = doc.Spreadsheet
-        print("mode = "+str(sheet.getEditorMode("A5")))
-        sheet.setEditorMode("A5", 0)
-        print("mode' = "+str(sheet.getEditorMode("A5")))
-        print("arclen = "+str(sheet.arclen))
-        print("radius = "+str(sheet.radius))
-        print("angle = "+str(sheet.angle))
-        print("width = "+str(sheet.width))
-        print("height = "+str(sheet.height))
-        sheet.set("arclen", str(arclen))
-        sheet.set("radius", str(radius))
-        sheet.set("width", str(width))
-        sheet.set("height", str(height))
-        sheet.recompute()
-        # verify that the radius and also the dependent angle have changed after the recomputer
-        print("arclen' = "+str(sheet.arclen))
-        print("radius' = "+str(sheet.radius))
-        print("angle' = "+str(sheet.angle))
-        print("width' = "+str(sheet.width))
-        print("height' = "+str(sheet.height))
-
-        # recompute the model
-        doc.recompute()
-
-        geometry = generate_geometry(doc)
-        print("generated geometry: "+str(geometry[0][0]))
-
-        assert geometry[0][0][2] == Vector(width, 0, 0)
-
-def generate_geometry(doc):
-    objects = doc.Objects
-    return [tessellate_shape(shaped) for shaped in objects if shaped.TypeId == 'PartDesign::Body']
-
-def tessellate_shape(shaped):
-    return shaped.Shape.tessellate(0.1)
-
-def main():
-    filename=sys.argv[1]
-    arclen=float(sys.argv[2])
-    radius=float(sys.argv[3])
-    width=float(sys.argv[4])
-    height=float(sys.argv[5])
-    convert_model(filename, arclen, radius, width, height)
-
-if __name__=='__main__':
-   main()
+## Generate bowls (based actual barrel diameter)
+for n in bowl_d:
+    sheet.set("barrel_d", n)
+    doc.recompute(None,True,True)
+    export_stl("Body003", "rs-winder-bowl-"+n+"mm.stl")
 
