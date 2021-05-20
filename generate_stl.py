723049d35c42927f4b6ea167cf7377e106ccea81
Vishnu P. Nambiar <5123344+vishnu350@users.noreply.github.com>
2021-05-21 01:03:40 +0800

    Updated generate flow and added 11.8mm barrel bowl size.

--- generate_stl.py
+++ generate_stl.py
@@ -4,6 +4,7 @@ sys.path.append(FREECADPATH)
 from collections import defaultdict
 import FreeCAD
 import Mesh
+import os
 
 def export_stl(body_obj,filename):
     __objs__=[]
@@ -12,7 +13,7 @@ def export_stl(body_obj,filename):
     del __objs__
 
 ## List of sizes to generate
-bowl_d   = ["10.0", "10.5", "11.0", "11.5", "12.0", "12.5", "13.0", "13.5", "14.0", "14.5", "15.0"]
+bowl_d   = ["10.0", "10.5", "11.0", "11.5", "11.8", "12.0", "12.5", "13.0", "13.5", "14.0", "14.5", "15.0"]
 spring_d = ["8.0",  "8.5",  "9.0",  "9.5",  "10.0", "10.5", "11.0", "11.5", "12.0", "12.5", "13.0"]
 
 ## Open project file and spreadsheet
@@ -20,8 +21,15 @@ FreeCAD.openDocument("rs-winder.FCStd")
 doc = App.ActiveDocument
 sheet = doc.Spreadsheet002
 
+## Create dirs
+os.mkdir("Release")
+os.mkdir("Release/winder-base")
+os.mkdir("Release/housing-barrel")
+os.mkdir("Release/barrel-bowl")
+os.mkdir("Release/plunger")
+
 ## Generate winder base
-export_stl("Body002", "rs-winder-base-stdsize.stl")
+export_stl("Body002", "Release/winder-base/rs-winder-base-stdsize.stl")
 
 ## Generate housing/plunger (based on spring diameter)
 for n in spring_d:
@@ -32,12 +40,12 @@ for n in spring_d:
     else:
         sheet.set("version_x_offs", "-6.60")
     doc.recompute(None,True,True)
-    export_stl("Body", "rs-winder-plunger-"+n+"mm.stl")
-    export_stl("Body001", "rs-winder-housing-"+n+"mm.stl")
+    export_stl("Body", "Release/plunger/rs-winder-plunger-"+n+"mm.stl")
+    export_stl("Body001", "Release/housing-barrel/rs-winder-housing-"+n+"mm.stl")
 
 ## Generate bowls (based actual barrel diameter)
 for n in bowl_d:
     sheet.set("barrel_d", n)
     doc.recompute(None,True,True)
-    export_stl("Body003", "rs-winder-bowl-"+n+"mm.stl")
+    export_stl("Body003", "Release/barrel-bowl/rs-winder-bowl-"+n+"mm.stl")
 
