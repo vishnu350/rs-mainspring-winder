771014748e5c8baffcde99848bd8319c32b8a74d
Vishnu <5123344+vishnu350@users.noreply.github.com>
2024-04-11 02:22:51 +0800

    Updated model to include parameterized fillet at barrel wall edge for better print quality.

--- generate_stl.py
+++ generate_stl.py
@@ -17,7 +17,7 @@ def export_mesh(body_obj,filename):
 ## List of sizes to generate
 ## Large range  = h=2.8, d=12-18
 ## Normal range = h=1.6, d=7-12
-rs_version = "0.96"
+rs_version = "0.97"
 spring1_d = ["7.0", "7.5", "8.0",  "8.5",  "9.0",  "9.5",  "10.0", "10.5", "11.0", "11.5", "12.0", "12.5"]
 spring2_d = ["13.0",  "13.5",  "14.0",  "14.5", "15.0", "15.5", "16.0", "16.5", "17.0", "17.5", "18.0", "18.5"]
 
